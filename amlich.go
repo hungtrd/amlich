041e97cf075df3e6a7ba7d267270823f38f12b5f
hungtd <hungtd@hblab.vn>
2023-02-23 17:26:21 +0700

    update test

--- amlich.go
+++ amlich.go
@@ -1,39 +1,29 @@
 package amlich
 
-import "math"
+import (
+	"math"
+)
 
 const pi float64 = 3.141592653589793
-
-func floor(f float64) int64 {
-	return int64(math.Floor(f))
-}
 
 func i2f(i int) float64 {
 	return float64(i)
 }
 
-// func date2JuliusDay(dd, mm, yy int) int {
-// 	a := (14 - i2f(mm)) / 12
-// 	y := i2f(yy) + 4800 - a
-// 	m := i2f(mm) + 12*a - 3
-// 	jd := i2f(dd) + (153*m+2)/5 + 365*y + y/4 - y/100 + y/400 - 32045
-// 	if jd < 2299161 {
-// 		jd = i2f(dd) + (153*m+2)/5 + 365*y + y/4 - 32083
-// 	}
-
-//		return int(jd)
-//	}
+func floor(f float64) int {
+	return int(math.Floor(f))
+}
 
 func date2JuliusDay(dd, mm, yy int) int {
-	a := (14 - mm) / 12
-	y := yy + 4800 - a
-	m := mm + 12*a - 3
-	jd := dd + (153*m+2)/5 + 365*y + y/4 - y/100 + y/400 - 32045
+	a := floor((14 - i2f(mm)) / 12)
+	y := i2f(yy) + 4800 - i2f(a)
+	m := i2f(mm) + 12*i2f(a) - 3
+	jd := i2f(dd) + math.Floor((153*m+2)/5) + 365*y + math.Floor(y/4) - math.Floor(y/100) + math.Floor(y/400) - 32045
 	if jd < 2299161 {
-		jd = dd + (153*m+2)/5 + 365*y + y/4 - 32083
+		jd = i2f(dd) + math.Floor((153*m+2)/5) + 365*y + math.Floor(y/4) - 32083
 	}
 
-	return int(jd)
+	return floor(jd)
 }
 
 func juliusDay2Date(jd int) (day, month, year int) {
@@ -60,7 +50,7 @@ func juliusDay2Date(jd int) (day, month, year int) {
 
 func newMoon(k int) float64 {
 	var kf, t, t2, t3, dr, jd1, m, mpr, f, c1, deltat, jdNew float64
-	kf = float64(k)
+	kf = i2f(k)
 	t = kf / 1236.85 // Time in Julian centuries from 1900 January 0.5
 	t2 = t * t
 	t3 = t2 * t
@@ -87,8 +77,8 @@ func newMoon(k int) float64 {
 	return jdNew
 }
 
-func getNewMoonDay(k int, tz float64) int {
-	return int(math.Floor(newMoon(k) + 0.5 + tz/24))
+func getNewMoonDay(k, tz int) int {
+	return floor(newMoon(k) + 0.5 + i2f(tz)/24)
 }
 
 func sunLongitude(jdn float64) float64 {
@@ -99,52 +89,52 @@ func sunLongitude(jdn float64) float64 {
 	m = 357.52910 + 35999.05030*t - 0.0001559*t2 - 0.00000048*t*t2 // mean anomaly, degree
 	l0 = 280.46645 + 36000.76983*t + 0.0003032*t2                  // mean longitude, degree
 	dl = (1.914600 - 0.004817*t - 0.000014*t2) * math.Sin(dr*m)
-	dl = dl + (0.019993-0.000101*t)*math.Sin(dr*2*m) + 0.000290*math.Sin(dr*3*m)
+	dl = dl + (0.019993-0.000101*t)*math.Sin(dr*2*m) + 0.00029*math.Sin(dr*3*m)
 	l = l0 + dl // true longitude, degree
 	l = l * dr
 	l = l - pi*2*(math.Floor(l/(pi*2))) // Normalize to (0, 2*PI)
 	return l
 }
 
-func getSunLongitude(d int, tz float64) int {
-	return int(math.Floor(sunLongitude(float64(d)-0.5-tz/24) / pi * 6))
+func getSunLongitude(d, tz int) int {
+	return floor((sunLongitude(i2f(d)-0.5-i2f(tz)/24) / pi) * 6)
 }
 
 /* Find the day that starts the luner month 11 of the given year for the given time zone */
-func getLunarMonth11(yy int, timeZone float64) int {
+func getLunarMonth11(yy, tz int) int {
 	var off, k, nm int
 
 	off = date2JuliusDay(31, 12, yy) - 2415021
-	k = int(math.Floor(float64(off) / 29.530588853))
-	nm = getNewMoonDay(k, timeZone)
-	sunLong := getSunLongitude(nm, timeZone) // sun longitude at local midnight
+	k = floor(i2f(off) / 29.530588853)
+	nm = getNewMoonDay(k, tz)
+	sunLong := getSunLongitude(nm, tz) // sun longitude at local midnight
 	if sunLong >= 9 {
-		nm = getNewMoonDay(k-1, timeZone)
+		nm = getNewMoonDay(k-1, tz)
 	}
 	return nm
 }
 
-func getLeapMonthOffset(a11, timeZone float64) int {
-	k := int(math.Floor((a11-2415021.076998695)/29.530588853 + 0.5))
+func getLeapMonthOffset(a11 float64, tz int) int {
+	k := floor((a11-2415021.076998695)/29.530588853 + 0.5)
 	last := 0
 	i := 1 // We start with the month following lunar month 11
-	arc := getSunLongitude(getNewMoonDay(k+i, timeZone), timeZone)
+	arc := getSunLongitude(getNewMoonDay(k+i, tz), tz)
 	for {
 		last = arc
-		i++
-		arc = getSunLongitude(getNewMoonDay(k+i, timeZone), timeZone)
+		i = i + 1
+		newmoon := getNewMoonDay(k+i, tz)
+		arc = getSunLongitude(newmoon, tz)
 
-		if arc != last && i < 14 {
+		if arc == last || i >= 14 {
 			break
 		}
 	}
 	return i - 1
 }
 
-func Solar2Lunar(dd, mm, yy int, tz float64) (lunarD, lunarM, lunarY int, lunarLeap bool) {
-	// var k, dayNumber, monthStart, a11, b11, lunarDay, lunarMonth, lunarYear, lunarLeap;
+func Solar2Lunar(dd, mm, yy, tz int) (lunarD, lunarM, lunarY int, lunarLeap bool) {
 	dayNumber := date2JuliusDay(dd, mm, yy)
-	k := int(math.Floor((float64(dayNumber) - 2415021.076998695) / 29.530588853))
+	k := floor((i2f(dayNumber) - 2415021.076998695) / 29.530588853)
 	monthStart := getNewMoonDay(k+1, tz)
 	if monthStart > dayNumber {
 		monthStart = getNewMoonDay(k, tz)
@@ -159,11 +149,11 @@ func Solar2Lunar(dd, mm, yy int, tz float64) (lunarD, lunarM, lunarY int, lunarL
 		b11 = getLunarMonth11(yy+1, tz)
 	}
 	lunarD = dayNumber - monthStart + 1
-	diff := int(math.Floor((float64(monthStart) - float64(a11)) / 29))
+	diff := floor((i2f(monthStart) - i2f(a11)) / 29)
 	lunarLeap = false
 	lunarM = diff + 11
 	if b11-a11 > 365 {
-		leapMonthDiff := getLeapMonthOffset(float64(a11), tz)
+		leapMonthDiff := getLeapMonthOffset(i2f(a11), tz)
 		if diff >= leapMonthDiff {
 			lunarM = diff + 10
 			if diff == leapMonthDiff {
@@ -181,22 +171,22 @@ func Solar2Lunar(dd, mm, yy int, tz float64) (lunarD, lunarM, lunarY int, lunarL
 	return
 }
 
-func Lunar2Solar(lunarDay, lunarMonth, lunarYear, lunarLeap int, timeZone float64) (d, m, y int) {
+func Lunar2Solar(lunarDay, lunarMonth, lunarYear, lunarLeap, tz int) (d, m, y int) {
 	var k, a11, b11, off, leapOff, leapMonth, monthStart int
 	if lunarMonth < 11 {
-		a11 = getLunarMonth11(lunarYear-1, timeZone)
-		b11 = getLunarMonth11(lunarYear, timeZone)
+		a11 = getLunarMonth11(lunarYear-1, tz)
+		b11 = getLunarMonth11(lunarYear, tz)
 	} else {
-		a11 = getLunarMonth11(lunarYear, timeZone)
-		b11 = getLunarMonth11(lunarYear+1, timeZone)
+		a11 = getLunarMonth11(lunarYear, tz)
+		b11 = getLunarMonth11(lunarYear+1, tz)
 	}
-	k = int(math.Floor(0.5 + (float64(a11)-2415021.076998695)/29.530588853))
+	k = floor(0.5 + (i2f(a11)-2415021.076998695)/29.530588853)
 	off = lunarMonth - 11
 	if off < 0 {
 		off += 12
 	}
 	if b11-a11 > 365 {
-		leapOff = getLeapMonthOffset(float64(a11), timeZone)
+		leapOff = getLeapMonthOffset(i2f(a11), tz)
 		leapMonth = leapOff - 2
 		if leapMonth < 0 {
 			leapMonth += 12
@@ -207,7 +197,7 @@ func Lunar2Solar(lunarDay, lunarMonth, lunarYear, lunarLeap int, timeZone float6
 			off += 1
 		}
 	}
-	monthStart = getNewMoonDay(k+off, timeZone)
+	monthStart = getNewMoonDay(k+off, tz)
 
 	return juliusDay2Date(monthStart + lunarDay - 1)
 }
--- amlich_test.go
+++ amlich_test.go
@@ -1,14 +1,34 @@
 package amlich
 
 import (
-	"fmt"
 	"testing"
 )
 
-// func TestSolar2Lunar(t *testing.T) {
-// 	fmt.Println(Solar2Lunar(23, 2, 2023, 7.0))
-// }
+func TestSolar2Lunar(t *testing.T) {
+	testCases := []struct {
+		Solar []int
+		Lunar []int
+	}{
+		{
+			Solar: []int{23, 2, 2023, 7},
+			Lunar: []int{4, 2, 2023, 0},
+		},
+		{
+			Solar: []int{19, 1, 2023, 7},
+			Lunar: []int{28, 12, 2022, 0},
+		},
+		{
+			Solar: []int{31, 12, 2022, 7},
+			Lunar: []int{9, 12, 2022, 0},
+		},
+	}
 
-func TestDate2JuliusDay(t *testing.T) {
-	fmt.Println(date2JuliusDay(22, 2, 2023))
+	for _, v := range testCases {
+		d, m, y, leap := Solar2Lunar(v.Solar[0], v.Solar[1], v.Solar[2], v.Solar[3])
+
+		l := v.Lunar[3] == 1
+		if d != v.Lunar[0] || m != v.Lunar[1] || y != v.Lunar[2] || l != leap {
+			t.Errorf("Failed test case: %v\nExpect: %v\nGot: %v %v %v %v", v.Solar, v.Lunar, d, m, y, leap)
+		}
+	}
 }
